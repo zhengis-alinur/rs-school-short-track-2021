ddc90fc7233a0750e727d8fe4dc6b453340cb232
zhengis-alinur <zhengis.alinur.erzhan@gmail.com>
2021-04-09 23:37:29 +0600

    feat: solve last task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,29 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const nums = [];
+  for (let i = 0; i < matrix.length; i++) {
+    nums[i] = [];
+  }
+  const height = matrix.length;
+  const width = matrix[0].length;
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      let count = 0;
+      for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+          if (i >= 0 && i < height && j >= 0 && j < width && (i !== x || j !== y)) {
+            if (matrix[i][j]) {
+              count++;
+            }
+          }
+        }
+      }
+      nums[x][y] = count;
+    }
+  }
+  return nums;
 }
 
 module.exports = minesweeper;
